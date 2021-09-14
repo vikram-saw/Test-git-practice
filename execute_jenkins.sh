f8644983c1a5562864350c82c798d0b4ddd94f6a
vikram-saw <81460733+vikram-saw@users.noreply.github.com>
2021-09-14 12:08:51 +0530

    create execute_jenkins.sh

---
+++ execute_jenkins.sh
@@ -0,0 +1,14 @@
+echo "BUILD_NUMBER" :: $BUILD_NUMBER
+echo "BUILD_ID" :: $BUILD_ID
+echo "BUILD_DISPLAY_NAME" :: $BUILD_DISPLAY_NAME
+echo "JOB_NAME" :: $JOB_NAME
+echo "EXECUTOR_NUMBER" :: $EXECUTOR_NUMBER
+echo "NODE_NAME" :: $NODE_NAME
+echo "NODE_LABELS" :: $NODE_LABELS
+echo "WORKSPACE" :: $WORKSPACE
+echo "JENKINS_HOME" :: $JENKINS_HOME
+echo "JENKINS_URL" :: $JENKINS_URL
+echo "BUILD_URL" ::$BUILD_URL
+echo "JOB_URL" :: $JOB_URL
+echo "Below output is all the environment variable in Jenkins"
+printenv
